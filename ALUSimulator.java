ccd96592c8e3d5189199f9df0c24e8097d716378
Erick Lopez <93694994+er-lo@users.noreply.github.com>
2024-07-16 12:58:25 -0500

    first commit

---
+++ ALUSimulator.java
@@ -0,0 +1,11 @@
+// Erick Lopez
+// Computer Systems
+// Summer 2024
+// 4-bit MIPS ALU Simulation in Java
+//
+
+public class ALUSimulator {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
